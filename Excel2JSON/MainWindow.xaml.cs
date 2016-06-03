da0e22874f8b59b7e291ca8320e25ac409d1dd62
tkdogan@cornell.edu <tkdogan@cornell.edu>
2016-06-03 20:19:35 +0200

    added interface for loading and converting Excel to JSON

---
+++ Excel2JSON/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Excel;
+
+namespace Excel2JSON
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenBth_Click(object sender, RoutedEventArgs e)
+        {
+
+
+
+
+            foreach (var worksheet in Workbook.Worksheets(@"C: \Users\Timur\Dropbox(Personal)\_CSHARP_PROJECTS\ArchsimLibraryData\160603_ExcelLibraryEditor.xlsx"))
+            {
+
+                foreach (var row in worksheet.Rows)
+                {
+
+
+                    foreach (var cell in row.Cells)
+                    {
+
+
+
+
+
+
+
+                    }
+
+                }
+
+
+            }
+
+
+
+
+        }
+    }
+}
